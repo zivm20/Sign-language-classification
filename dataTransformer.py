f00b5cffd4f2ea1109c9769e7fc67c75d11cc66e
Ziv Morgan <ziv.morgan@gmail.com>
2024-02-11 18:16:51 +0200

    final model tuning

--- dataTransformer.py
+++ dataTransformer.py
@@ -54,58 +54,96 @@ def create_transformation_layer():
     return transforms 
 
 class ResnetBlock(tf.keras.Model):
-    def __init__(self, kernel_size, filt, name=None):
+    def __init__(self, kernel_size, filt, filt_in=None,filt_out=None,num=3,conv_block=False,stride=1,bottleNeck=True, name=None):
         super(ResnetBlock, self).__init__(name=name)
         self.kernel_size = kernel_size
         self.filt = filt
+        self.num = num
+        self.bottleNck=bottleNeck
+        if filt_out == None and filt_in == None:
+            filt_out = filt*stride
+        elif filt_out == None:
+            filt_out = filt_in*stride
+        if filt_in != None:
+            filt_in = filt
+
+        self.filt_out = filt_out
+        self.filt_in = filt_in
+        self.conv_layers = []
+        self.norm_layers = []
+        self.stride = stride
+
+        self.conv_block = (self.filt_out != self.filt_in or stride>1 or conv_block)
+
+        if bottleNeck == True and num>2:
+            self.conv_layers.append(tf.keras.layers.Conv2D(self.filt, 1,strides=stride))
+        else:
+            self.conv_layers.append(tf.keras.layers.Conv2D(self.filt, kernel_size,padding='same',strides=stride))
+        self.norm_layers.append(tf.keras.layers.BatchNormalization())
         
+        
+        for i in range(1, num-1):
+            if bottleNeck == True and (i!=num//2 and i!=(num-1)//2):
+               self.conv_layers.append(tf.keras.layers.Conv2D(self.filt, 1))
+            else:
+                self.conv_layers.append(tf.keras.layers.Conv2D(self.filt, kernel_size, padding='same'))
+            self.norm_layers.append(tf.keras.layers.BatchNormalization())
+            
 
-        self.conv2a = tf.keras.layers.Conv2D(filt, (1, 1))
-        self.bn2a = tf.keras.layers.BatchNormalization()
+        if bottleNeck == True and num>2:
+            self.conv_layers.append(tf.keras.layers.Conv2D(self.filt_out, 1))
+        else:
+            self.conv_layers.append(tf.keras.layers.Conv2D(self.filt_out, kernel_size, padding='same'))
+        self.norm_layers.append(tf.keras.layers.BatchNormalization())
 
-        self.conv2b = tf.keras.layers.Conv2D(filt, kernel_size, padding='same')
-        self.bn2b = tf.keras.layers.BatchNormalization()
-
-        self.conv2c = tf.keras.layers.Conv2D(filt*2, (1, 1))
-        self.bn2c = tf.keras.layers.BatchNormalization()
+        self.x_shortcut = []
+        if self.conv_block:
+            self.x_shortcut = [tf.keras.layers.Conv2D(self.filt_out, kernel_size,padding='same',strides=stride),
+                               tf.keras.layers.BatchNormalization()]
+        
 
         
 
     def call(self, input_tensor, training=False):
-        x = self.conv2a(input_tensor)
-        x = self.bn2a(x, training=training)
-        x = tf.keras.layers.Activation('relu')(x)
+        x_in = input_tensor
+        x = input_tensor
 
-        x = self.conv2b(x)
-        x = self.bn2b(x, training=training)
-        x = tf.keras.layers.Activation('relu')(x)
+        for i in range(len(self.conv_layers)):
+            x = self.conv_layers[i](x)
+            x = self.norm_layers[i](x, training=training)
+            x = tf.keras.layers.Activation('relu')(x)
 
-        x = self.conv2c(x)
-        x = self.bn2c(x, training=training)
+        if self.conv_block:
+            x_in = self.x_shortcut[0](input_tensor)
+            x_in = self.x_shortcut[1](x_in,training=training)
 
-        x = x + input_tensor
+        x = x+x_in
+        
 
         return tf.keras.layers.Activation('relu')(x)
         
     def get_config(self):
-        return {"kernel_size": self.kernel_size, "filt":self.filt}
+        return {"kernel_size": self.kernel_size, "filt":self.filt,"filt_in":self.filt_in,"filt_out":self.filt_out,"num":self.num,"conv_block":self.conv_block,"stride":self.stride,"bottleNeck":self.bottleNeck}
 
 
 
 def create_model(num_classes,transformation):
     model = tf.keras.Sequential([
-        *transformation,
-        #tf.keras.layers.Conv2D(32, 7, activation='relu',padding='same'),
-        #ResnetBlock(7,16,'res1'),
-        tf.keras.layers.Conv2D(64, 5, activation='relu', padding='same',strides=2),#size/2
-        ResnetBlock(5,32,'res2'),
-        tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same',strides=2),#size/4
-        ResnetBlock(3,64,'res3'),
-        tf.keras.layers.Conv2D(256, 1, activation='relu', padding='same',strides=2),#size/8
+        transformation,
+        tf.keras.layers.Conv2D(32, 7,padding='same',strides=1),#56x56
+        ResnetBlock(5,filt=64,filt_in=32,num=2,stride=2),#28x28
+        ResnetBlock(5,filt=64,num=2),
+
+        ResnetBlock(3,filt=128,filt_in=64,num=2,stride=2),#14x14
+        ResnetBlock(3,filt=128,filt_in=128,num=2),
+
+        ResnetBlock(3,filt=256,filt_in=128,num=2,stride=2),#7x7
+        ResnetBlock(3,filt=256,filt_in=256,num=2),
+
+        tf.keras.layers.AveragePooling2D(7),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256*8,activation='relu'),
         tf.keras.layers.Dense(num_classes)
-    ])
+        ])
 
     model(tf.ones((1,*IMG_DIM)))
     return model
--- model_params.py
+++ model_params.py
@@ -6,7 +6,7 @@ import numpy as np
 BATCH_SIZE = 32
 IMG_DIM = (128,128,3)
 CLASS_NAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
-INPUT_SHAPE = (64,64,3)
+INPUT_SHAPE = (56,56,3)
 
 
 def preProcessing(img):
